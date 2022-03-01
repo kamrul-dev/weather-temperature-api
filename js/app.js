c8e3c35234f1db76f7a0dd3f06fba47a169761da
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-01 20:59:52 +0600

    fetch the data with api key

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 // only practice purspose
-const API_KEY = '65c2f249afbd5541c418dc6a331f5ae8'
+const API_KEY = '65c2f249afbd5541c418dc6a331f5ae8';
+const SearchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`;
+    console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
