d78644474791e2a2edf89aaced5b683c2c038825
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-01 21:48:32 +0600

    data loaded on the UI

--- js/app.js
+++ js/app.js
@@ -2,10 +2,26 @@
 const API_KEY = '65c2f249afbd5541c418dc6a331f5ae8';
 const SearchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     console.log(url);
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayTemperature(data));
+}
+
+// display weather data on UI
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    console.log(temperature);
+
+    // weather icon set 
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imageIcon = document.getElementById('weather-icon');
+    imageIcon.setAttribute('src', url);
 }
